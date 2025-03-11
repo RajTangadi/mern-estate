b919686283e4a3645f38e3c05542f0258755b578
Raj Tangadi <rajtangadi65@gmail.com>
2025-03-12 03:42:29 +0530

    Add Redux Persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { store } from './redux/store.js'
-import { Provider } from 'react-redux'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { persistore, store } from "./redux/store.js";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-
-createRoot(document.getElementById('root')).render(
-  <Provider store={store}> 
-  <StrictMode>
-    <App />
-  </StrictMode>
-  </Provider>,
-)
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistore}>
+      <StrictMode>
+        <App />
+      </StrictMode>
+    </PersistGate>
+  </Provider>
+);
