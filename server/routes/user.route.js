e0313320014ffe9be508a483a882de3ba0c2cb5e
Raj Tangadi <rajtangadi65@gmail.com>
2025-03-21 14:14:01 +0530

    create get user listing api route

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { updateUser, deleteUser } from "../controllers/user.controller.js";
+import { updateUser, deleteUser, getUserListings } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 import uploadMiddleware from "../utils/uploadMiddleware.js";
 
@@ -7,6 +7,7 @@ const router = Router();
 
 router.post("/update/:id", verifyToken, uploadMiddleware, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
-router.get("/listings/:id", verifyToken);
+router.get("/listings/:id", verifyToken, getUserListings);
+
 // getUserListings
 export default router;
