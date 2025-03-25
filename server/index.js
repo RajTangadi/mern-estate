34453726f6edc1a9ed3fd79fab09c54e5f8370c6
Raj Tangadi <rajtangadi65@gmail.com>
2025-03-25 15:35:28 +0530

    add pre installation

--- server/index.js
+++ server/index.js
@@ -6,10 +6,14 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.router.js";
 import listingRouter from "./routes/listing.route.js";
 import { v2 as cloudinary } from "cloudinary";
+import path from "path";
 import cors from "cors";
-// import uploadRoutes from "./routes/uploadRoutes.js";
+// import  uploadRoutes from "./routes/uploadRoutes.js";
 
 dotenv.config();
+
+const __dirname = path.resolve();
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
@@ -27,6 +31,11 @@ app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 // app.use('/api/user', uploadRoutes);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
