fff5a9af406dc53646f66c8c1f92f3695af9c295
Raj Tangadi <rajtangadi65@gmail.com>
2025-03-25 20:14:02 +0530

    fix bug

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Signin from "./pages/Signin";
 import SignUp from "./pages/SignUp";
-import About from "./pages/about";
+import About from "./pages/About";
 import Profile from "./pages/Profile";
 import Header from "./components/Header";
 import { ToastContainer } from "react-toastify";
